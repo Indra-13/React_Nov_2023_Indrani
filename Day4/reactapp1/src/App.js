4047fa5dd7792e183bc3191f1b17cdc162fe48b6
Indra-13 <124855942+Indra-13@users.noreply.github.com>
2023-11-16 16:27:21 +0530

    DAY4

---
+++ Day4/reactapp1/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Listitem from "./components/list/Listitem";
+function App() {
+  return (
+    <div className="App">
+      <Listitem />
+    </div>
+  );
+}
+
+export default App;
