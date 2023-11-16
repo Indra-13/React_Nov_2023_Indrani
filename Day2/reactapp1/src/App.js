fe0355cd3520e91677af8431f16faa18cfc9d985
Indra-13 <124855942+Indra-13@users.noreply.github.com>
2023-11-16 16:25:11 +0530

    DAY2

---
+++ Day2/reactapp1/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Stylish from './components/Stylish'
+function App() {
+  return (
+    <div className="App">
+      <Stylish/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day2/reactapp3/src/components/Example.js
@@ -0,0 +1,15 @@
+
+const Example = () =>{
+    return (
+        <div>
+            <h1>About us</h1>
+            <hr></hr>
+            <p>
+            React is a JavaScript library for building user interfaces.
+            React is used to build single-page applications.
+            React allows us to create reusable UI components.
+            </p>
+        </div>
+    );
+};
+export default Example;
